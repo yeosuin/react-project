61ed9c8bbf81b1d6c73770911727fa1971b4f97c
suin <immof@naver.com>
2025-06-23 17:20:26 +0900

    배열메서드 / 비동기 작업 처리하기 2. Promise

---
+++ section2/chapter13.js
@@ -0,0 +1,61 @@
+function add10(num) {
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업 실행하는 함수
+    // executor
+    setTimeout(() => {
+      if(typeof  num === 'number') {
+        resolve(num + 10);
+      } else {
+        reject('num이 숫자가 아닙니다');
+      }
+    }, 2000);
+  })
+
+  return promise;
+}
+
+add10(0).then((result) => {
+  console.log(result);
+  return add10(result);
+}).then((result) => {
+  console.log(result);
+  return add10(undefined);
+}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+})
+
+/*const promise = new Promise((resolve, reject) => {
+  // 비동기 작업 실행하는 함수
+  // executor
+  setTimeout(() => {
+    const num = 10;
+    if(typeof  num === 'number') {
+      resolve(num + 100);
+    } else {
+      reject('num이 숫자가 아닙니다');
+    }
+  }, 2000);
+})
+
+// then 메서드
+// -> 그 이후에
+// 성공했을 때만 실행됨
+promise.then((value) => {
+  console.log(value)
+})
+
+//promoise chaning
+//promise 똑같은 개체라서 같이 써 줘도 됨
+promise.then((value) => {
+  console.log(value)
+}).catch((error) => {
+  console.log(error);
+})
+
+// catch 메서드
+// 실패했을 때 실행됨
+promise.catch((error) => {
+  console.log(error);
+})*/
