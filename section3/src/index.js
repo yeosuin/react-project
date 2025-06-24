f5e62ef931932020df0d5cef9ab25fde608b7ec6
suin <immof@naver.com>
2025-06-24 10:24:32 +0900

    Node.js 라이브러리 사용하기

--- section3/src/index.js
+++ section3/src/index.js
@@ -1,6 +1,12 @@
 import mul, { add, sub } from './math.js';
+import randomColor from 'randomcolor';
 
 console.log(add(1, 2));
 console.log(sub(1, 2));
 console.log(mul(1, 2));
+
 console.log('hi');
+
+const color = randomColor();
+console.log(color);
+
