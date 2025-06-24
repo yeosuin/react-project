5860f8215bf901e572feda0e44c0e43cb515b1e9
suin <immof@naver.com>
2025-06-24 10:14:14 +0900

    Node.js 모듈 시스템 이해하기

--- section3/src/index.js
+++ section3/src/index.js
@@ -1,1 +1,6 @@
+import mul, { add, sub } from './math.js';
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
 console.log('hi');
---
+++ section3/src/math.js
@@ -0,0 +1,23 @@
+// math 모듈
+
+export function  add(a, b) {
+  return a + b;
+}
+
+export function  sub(a, b) {
+  return a - b;
+}
+
+/*
+module.exports = {
+  add,
+  sub,
+};*/
+
+/*
+export { add, sub };*/
+
+
+export  default function  multiply(a, b) {
+  return a * b;
+}
