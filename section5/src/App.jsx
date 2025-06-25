e341914f9542ede5fd35bfb8eedb32ea4ae768e0
suin <immof@naver.com>
2025-06-25 09:42:25 +0900

    React 컴포넌트

--- section5/src/App.jsx
+++ section5/src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header.jsx';
+import Main from './components/Mian.jsx';
+import Footer from './components/Footer.jsx';
 
-function App() {
+// 컴포넌트 함수명은 반드시 첫 글자가 대문자여야 함
+// 여기서 App은 root 컴포넌트라고 할 수 있음 모든 컴포넌트의 조상 역할임
+
+/*
+const Header = () => { // 자식 컴포넌트
+  return (
+      <header>
+        <h1>Header</h1>
+      </header>
+  )
+}
+*/
+
+function App() { // 부모 컴포넌트
 
   return (
     <>
-      <h1> hi 리액트 </h1>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
 
-export default App
+export default App;
