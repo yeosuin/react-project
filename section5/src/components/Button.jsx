f9c4bb09d5773083053e630fa971946cccb7b255
suin <immof@naver.com>
2025-06-25 13:59:49 +0900

    이벤트 처리하기

--- section5/src/components/Button.jsx
+++ section5/src/components/Button.jsx
@@ -1,8 +1,16 @@
 const Button = ({children, text, color = 'black'}) => {
   //props를 구조분해할당 문법으로 가지고 와야 함
   // 기본값을 위에 설정해 주면 됨
+
+  const onClickButton = (e) => {
+    console.log(e);
+  }
   return (
-      <button style={{ color: color}}>
+      <button
+          //이벤트 핸들러
+          onClick={onClickButton}
+          /*onMouseEnter={onClickButton}*/
+          style={{ color: color}}>
     {text} - {color.toUpperCase()}
         {children}
   </button>
