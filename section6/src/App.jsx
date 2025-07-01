3d0a8b68ce177e8a6cd1d76fc790c6849c4fcab8
suin <immof@naver.com>
2025-07-01 10:35:16 +0900

    기능 구현하기

--- section6/src/App.jsx
+++ section6/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css';
 import Controller from './components/Controller.jsx';
 import Viewer from './components/Viewer.jsx';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
   return (
       <div className='App'>
         <h1>Simple Counter</h1>
         <section>
-           <Viewer/>
+           <Viewer count={count}/>
         </section>
         <section>
-           <Controller/>
+           <Controller onClickButton={onClickButton}/>
         </section>
       </div>
   );
