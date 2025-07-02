f7ea15f306814913886d6085244920b88fc14c4d
suin <immof@naver.com>
2025-07-02 15:53:17 +0900

    Todolist UI 구현하기

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header.jsx';
+import Editor from './components/Editor.jsx';
+import List from './components/List.jsx';
+import TodoItem from './components/TodoItem.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      Todolist
-    </>
+    <div className='App'>
+      <Header/>
+      <Editor/>
+      <List/>
+    </div>
   )
 }
 
