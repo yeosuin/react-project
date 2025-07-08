e0279d8e2a9ba7b5ebd834c2a99dfba49e3c15b6
suin <immof@naver.com>
2025-07-08 16:23:43 +0900

    기능 구현 준비

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
 import List from './components/List.jsx';
 import TodoItem from './components/TodoItem.jsx';
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습',
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState(mockData);
 
   return (
-    <div className='App'>
-      <Header/>
-      <Editor/>
-      <List/>
-    </div>
-  )
+      <div className="App">
+        <Header/>
+        <Editor/>
+        <List/>
+      </div>
+  );
 }
 
-export default App
+export default App;
