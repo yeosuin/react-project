5daf4c7e318670f9f124a08d3ac18097b78c0fbd
suin <immof@naver.com>
2025-07-08 16:39:50 +0900

    투두 추가하기

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
@@ -29,11 +29,24 @@ const mockData = [
 function App() {
 
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+
+    setTodos([newTodo, ...todos])
+  }
+
 
   return (
       <div className="App">
         <Header/>
-        <Editor/>
+        <Editor onCreate={onCreate}/>
         <List/>
       </div>
   );
