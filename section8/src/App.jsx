51b8b54a5dd7510717838051f7381cc7ce6f4af0
suin <immof@naver.com>
2025-07-11 16:53:50 +0900

    useReducer로 업그레이드

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import {useReducer, useRef, useState} from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
@@ -27,20 +27,35 @@ const mockData = [
   },
 ];
 
-function App() {
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE' :
+      return [action.data, ...state];
+    case 'UPDATE' :
+      return state.map((item) => item.id === action.targetId ?
+          {...item, isDone: !item.isDone} :
+          item);
+    case 'DELETE' :
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
 
-  const [todos, setTodos] = useState(mockData);
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
@@ -58,22 +73,25 @@ function App() {
       return todo;
     }))*/
     //위를 간결하게 바꾸기
-    setTodos(todos.map((todo) => todo.id === targetId ?
-        {...todo, isDone: !todo.isDone} :
-        todo));
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
     // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId));
-  }
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    })
+  };
 
   return (
       <div className="App">
-        <Exam/>
-        {/*<Header/>
+        <Header/>
         <Editor onCreate={onCreate}/>
-        <List todos={todos} onUpdate={onUpdate}/>*/}
+        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
       </div>
   );
 }
